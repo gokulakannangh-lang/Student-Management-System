ec6c7d3d31e977da7e68a10d083fd77b2b04547e
Gokulakannan A <gokulakannangh@gmail.com>
2026-08-30 04:53:06 +0530

    Fix admin table name

--- backend/server.js
+++ backend/server.js
@@ -43,7 +43,7 @@ app.post("/login", (req, res) => {
 
       const adminSql = `
         SELECT *
-        FROM admins
+        FROM admin
         WHERE username = ?
         AND password = ?
         LIMIT 1
